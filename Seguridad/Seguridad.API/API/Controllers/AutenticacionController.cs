41396c85ab2c04b917654b878fc103a1d8252788
Joawong <49043721+Joawong@users.noreply.github.com>
2026-04-16 19:06:16 -0600

    API Seguridad

---
+++ Seguridad/Seguridad.API/API/Controllers/AutenticacionController.cs
@@ -0,0 +1,28 @@
+﻿using Abstracciones.API;
+using Abstracciones.Flujo;
+using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutenticacionController : ControllerBase, IAutenticacionController
+    {
+        private IAutenticacionFlujo _autenticacionFlujo;
+
+        public AutenticacionController(IAutenticacionFlujo autenticacionFlujo)
+        {
+            _autenticacionFlujo = autenticacionFlujo;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> PostAsync([FromBody] LoginBase login)
+        {
+            return Ok(await _autenticacionFlujo.LoginAsync(login));
+        }
+    }
+}
